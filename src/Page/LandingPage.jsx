110b24aac2f64424056c3ce042100b75e58618ce
madoelmalah4 <152697670+madoelmalah4@users.noreply.github.com>
2024-12-11 09:33:11 +0200

    commit changes

--- src/Page/LandingPage.jsx
+++ src/Page/LandingPage.jsx
@@ -1,30 +1,42 @@
-import React, { useEffect, useState } from "react";
-import { Box, Hidden, Stack, Typography } from "@mui/material";
+import React, { useEffect, useRef, useState } from "react";
+import { Box, CircularProgress, Stack, Tooltip } from "@mui/material";
 import Navbar from "../Components/Navbar";
 import { motion } from "framer-motion";
 import AnimatedText from "../Components/AnimatedText";
-import img1 from '../assets/img.jpg'
-import { Parallax , ParallaxLayer } from "@react-spring/parallax";
-const LandingPage = () => {
-  const [mouseHover, setMousehover] = useState({
-    x: 0,
-    y: 0,
-  });
+import img1 from "../assets/img.jpg";
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 
-  const [str, setStr] = useState("Transforming Ideas into Digital Reality");
+const LandingPage = () => {
+  const InpRef = useRef(null); 
+  const [mouseHover, setMouseHover] = useState({ x: 0, y: 0 });
+  const [bgColor, setBgColor] = useState("transparent");
 
   useEffect(() => {
     const mouseMove = (e) => {
-      setMousehover({
-        x: e.clientX,
-        y: e.clientY,
-      });
+      setMouseHover({ x: e.clientX, y: e.clientY });
     };
-
     window.addEventListener("mousemove", mouseMove);
 
     return () => {
       window.removeEventListener("mousemove", mouseMove);
+    };
+  }, []);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollOffset = InpRef.current.container.current.scrollTop / window.innerHeight;
+      if (scrollOffset >= 1 && scrollOffset < 2) {
+        setBgColor("white"); 
+      } else {
+        setBgColor("transparent");
+      }
+    };
+
+    const container = InpRef.current.container.current;
+    container.addEventListener("scroll", handleScroll);
+
+    return () => {
+      container.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
@@ -36,53 +48,81 @@ const LandingPage = () => {
   };
 
   return (
-    <Parallax pages={4}>
-      <ParallaxLayer speed={0.3} factor={0} offset={1} >
-      <Stack
-        sx={{
-          width: "100vw",
-          height: "100vh",
-        }}
-      >
-      <Navbar />
-      <motion.div
-        style={{
-          backgroundColor: "transparent",
-          borderRadius: "50%",
-          width: "25px",
-          height: "25px",
-          border: "1px solid white",
-          zIndex:9999
-        }}
-        variants={variants}
-        animate="default"
-      />
+    <Parallax pages={4} ref={InpRef}>
+      <ParallaxLayer speed={0.2} factor={1} offset={0}>
+        <Stack
+          sx={{
+            width: "100vw",
+            height: "100%",
+            backgroundColor: bgColor,
+          }}
+        >
+          <Navbar />
+          {/* <motion.div
+            style={{
+              backgroundColor: "transparent",
+              borderRadius: "50%",
+              width: "25px",
+              height: "25px",
+              border: "1px solid white",
+              zIndex: 9999,
+            }}
+            variants={variants}
+            animate="default"
+          /> */}
 
-      <Stack sx={{
-        alignItems:"center",
-        width:"90%",
-        borderRadius:"30px",
-        height:"100%",
-        mt:5,
-        ml:13,
-        p:4,
-      }}>
-       <AnimatedText str={"Empower Your Business with Stunning, High-Performance Websites!"} fontSize={70} speed={0.003}/>
-       <Box component={"img"}
-       src={img1}
-       sx={{
-        width:"900px",
-        borderRadius:"30px",
-        transition:"0.3s ease-out",
-        "&:hover":{
-          scale:"1.01"
-        }
-       }}
-       />
-      </Stack>
-    </Stack>
-    </ParallaxLayer>
+          <Stack
+            sx={{
+              alignItems: "center",
+              width: "90%",
+              borderRadius: "30px",
+              height: "100%",
+              mt: 5,
+              ml: 13,
+              p: 4,
+              justifyContent: "start",
+              gap:4
+            }}
+          >
+            <AnimatedText
+              str={"Empower Your Business with Stunning, High-Performance Websites!"}
+              fontSize={70}
+              speed={0.003}
+            />
+            <Tooltip title="Go to Introduction" arrow={true} placement="top"  sx={{
+              padding:3
+            }}>
+            <Box
+              component={"img"}
+              src={img1}
+              sx={{
+                width: "900px",
+                borderRadius: "30px",
+                transition: "0.3s ease-out",
+                "&:hover": {
+                  scale: "1.01",
+                  boxShadow: "7px 7px 0px #2575FC",
+                },
+              }}
+              onClick={() => InpRef.current.scrollTo(1.3)}
+              />
+              </Tooltip>
+          </Stack>
+        </Stack>
+      </ParallaxLayer>
 
+      <ParallaxLayer speed={0.2} factor={2} offset={0.5}>
+        <Stack
+          sx={{
+            backgroundColor: bgColor,
+            width: "100vw",
+            height: "100%",
+            transition:"0.3s ease"
+          }}
+        >
+
+        </Stack>
+      </ParallaxLayer>
     </Parallax>
   );
 };
